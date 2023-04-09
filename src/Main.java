64cc548fb180755ea7e9ee35de98dfaaaea5e854
aizeresalim <0424aizeka@gmail.com>
2023-04-09 14:56:52 +0600

    task 7

--- src/Main.java
+++ src/Main.java
@@ -31,6 +31,27 @@ public class Main {
                 int result = task6.solve(a, n);
                 System.out.println(a + " raised to the power of " + n + " is " + result);
                 break;
+            case 7:
+                System.out.print("Enter the size of the array: ");
+                int arraySize = sc.nextInt();
+                int[] arr = new int[arraySize];
+                System.out.print("Enter the elements of the array: ");
+                for (int i = 0; i < arraySize; i++) {
+                    arr[i] = sc.nextInt();
+                }
+
+                System.out.println("Original array:");
+                for (int i = 0; i < arraySize; i++) {
+                    System.out.print(arr[i] + " ");
+                }
+
+                task7.solve(arr, 0, arraySize - 1);
+                System.out.println("\nReversed array:");
+                for (int i = 0; i < arraySize; i++) {
+                    System.out.print(arr[i] + " ");
+                }
+                break;
+
             default:
                 System.out.println("Invalid task number");
                 break;
---
+++ src/task7.java
@@ -0,0 +1,17 @@
+public class task7 {
+
+    public static void solve(int[] arr, int start, int end) {
+        if (start >= end) { // Base case: array is already reversed
+            return;
+        }
+
+        // Swap the elements at start and end positions
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        // Recursive call with start incremented and end decremented
+        solve(arr, start + 1, end - 1);
+    }
+}
+
